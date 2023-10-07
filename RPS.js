d0df74ffeae6b713541a74125eecc53d6d957894
Mkhabele Mikejunior Mark Mashaole <137772011+mkhmik004@users.noreply.github.com>
2023-10-07 13:15:16 +0200

    Update RPS.js

--- RPS.js
+++ RPS.js
@@ -1,26 +1,29 @@
-//computer random selection from list of string
+function game(){
+    //computer random selection from list of string
 const choice=["rock" , "paper", "scissors"];
 function getComputerChoice() {
     return choice[Math.floor(((Math.random()))*3)];
 }
-
 //assign selections to variables
 let computerS=getComputerChoice();
 let playerS=prompt("rock,paper scissor shooot");
-console.log();
 //compare computer string and player string
 function playRound(playerSelection, computerSelection){
    if (playerSelection.toLowerCase()===computerSelection){
     console.log("Tie");
    }
    else if (playerSelection.toLowerCase()==="rock" && computerSelection==="scissor" || playerSelection==="paper" && computerSelection==="rock" || playerSelection==="scissor" && computerSelection==="paper" ){
-    console.log("ohhh, you Win this round")
+    console.log("ohhh, you Win this round");
    }
    else{
-    console.log("Oppps, you lose")
+    console.log("Oppps, you lose");
    }
 }
-playRound(playerS,computerS,)
+playRound(playerS,computerS);
+}
+for (i=0;i<6;i++){
+    game();
+}
 
 
 
